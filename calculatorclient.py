ce6a05dada8b1df61be5758250aadc77ad88d681
MrEmmirul21 <emmirulmirul@gmail.com>
2022-01-02 11:47:57 +0800

    lab6

---
+++ calculatorclient.py
@@ -0,0 +1,38 @@
+import socket
+
+ClientSocket = socket.socket()
+host = '192.168.170.14'
+port = 8888
+
+print('Waiting for connection')
+try:
+    ClientSocket.connect((host, port))
+except socket.error as e:
+    print(str(e))
+
+Response = ClientSocket.recv(2048)
+print(Response.decode("utf-8"))
+print('<<<Math calculator>>>')
+
+while True:
+    print('<<<Math calculator>>>')
+    print('Function available:')
+    print('a - Logarithmic(base-10)')
+    print('b - Square Root')
+    print('c - Exponential')
+    print('d - Factorial')
+    print('\nx - Exit\n')
+
+    func = input('Enter the function : ')
+
+    if func != 'x':
+        numb = input('Enter the number : ')
+        ClientSocket.send(str.encode(func+':'+numb))
+        Response = ClientSocket.recv(2048)
+        print('Answer = ')
+        print(Response.decode('utf-8'))
+
+    else:
+        break
+
+ClientSocket.close()
